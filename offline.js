81bcaf45dc06af31cfb4ffc24d99dc7a950ed3fc
guruchian <30399464+semicon@users.noreply.github.com>
2019-06-13 01:07:52 +0700

    puzzle
    puzzle

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1560362467,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/mainstate-sheet0.png",
+		"images/tristate-sheet0.png",
+		"images/dragtri-sheet0.png",
+		"images/sqstate-sheet0.png",
+		"images/dragsq-sheet0.png",
+		"images/rotater-sheet0.png",
+		"images/rotatel-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/sqbtn-sheet0.png",
+		"images/tribtn-sheet0.png",
+		"images/state-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
